69f73506f6ef110d139b44b3dd7d3ffedbdc4dee
Sebastian Gräßl <mail@bastilian.me>
2017-02-15 19:05:39 +0100

    WIP #18518 - Validate that image exists for compute_resource

--- app/models/docker_container_wizard_states/image.rb
+++ app/models/docker_container_wizard_states/image.rb
@@ -4,8 +4,16 @@ module DockerContainerWizardStates
     belongs_to :wizard_state, :class_name => DockerContainerWizardState,
                               :foreign_key => :docker_container_wizard_state_id
     delegate :compute_resource_id, :to => :wizard_state
+    delegate :compute_resource, :to => :wizard_state
 
     validates :tag,             :presence => true
     validates :repository_name, :presence => true
+    validate :validate_image_exists
+
+    def validate_image_exists
+      unless compute_resource.exist? "#{repository_name}:#{tag}"
+        errors.add(:image, "Docker Image does not exist!")
+      end
+    end
   end
 end
