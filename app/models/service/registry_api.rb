9e2d914a38dc449a41d7a3218177f22f4e65fca4
Sebastian Gräßl <mail@bastilian.me>
2017-02-24 11:56:21 +0100

    WIP #18518 - Add tests and improvements for RegistryApi
    This moves RegistryApi away from RestClient to Docker::Connection.
    It also improves the interface of the class to provide a simpler
    way to query images and their tags.
    
    To fix issues with unsupported api versions of registries it now
    tries both and returns a normalized format of search results.
    
    To query Docker Hub there is now RegistryApi.docker_hub to get an
    instance back configured for it.

--- app/models/service/registry_api.rb
+++ app/models/service/registry_api.rb
@@ -1,26 +1,75 @@
 module Service
   class RegistryApi
+    DOCKER_HUB = 'https://registry.hub.docker.com/'
     DEFAULTS = { :url => 'http://localhost:5000' }
-    attr_reader :config
+    attr_accessor :config, :url, :user, :password
 
     def initialize(params = {})
-      config = DEFAULTS.merge(params)
-      uri = URI(config.delete(:url))
-      uri.user = config.delete(:user) unless config[:user].blank?
-      uri.password = config.delete(:password) unless config[:password].blank?
-      @config = config.merge(:url => uri.to_s)
+      self.config = DEFAULTS.merge(params)
+      self.url = config[:url]
+      self.user = config[:user] unless config[:user].blank?
+      self.password = config[:password] unless config[:password].blank?
+
+      Docker.logger = Rails.logger if Rails.env.development?
     end
 
-    def search(aquery)
-      response = RestClient.get(config[:url] + '/v1/search',
-                                :params => { :q => aquery }, :accept => :json)
-      JSON.parse(response.body)
+    def connection
+      @connection ||= ::Docker::Connection.new(url, credentials)
     end
 
-    def list_repository_tags(arepository)
-      response = RestClient.get(config[:url] + "/v1/repositories/#{arepository}/tags",
-                                :accept => :json)
-      JSON.parse(response.body)
+    def get(path, params = nil)
+      json = connection.get('/', params, { omit_default_port: true,
+                                           path: "#{path}"  })
+      JSON.parse(json)
+    end
+
+    # Since the Registry API v2 does not support a search the v1 endpoint is used
+    # Newer registries will fail, the v2 catalog endpoint is used
+    def search(query)
+      get('/v1/search', { q: query })
+    rescue
+      { 'results' => catalog(query) }
+    end
+
+    # Some Registries might have this endpoint not implemented/enabled
+    def catalog(query)
+      get('/v2/_catalog')['repositories'].select do |image|
+        image =~ /^#{query}/
+      end.map { |image_name| { 'name' => image_name } }
+    end
+
+    def tags(image_name, query = nil)
+      begin
+        result = get("/v1/repositories/#{image_name}/tags")
+      rescue
+        result = tags_v2(image_name)
+      end
+      if result
+        result = filter_tags(result, query) if query
+        result
+      else
+        []
+      end
+    end
+
+    def tags_v2(image_name)
+      get("/v2/#{image_name}/tags/list")['tags'].map { |tag| { 'name' => tag } }
+    end
+
+    def self.docker_hub
+      @@docker_hub ||= new(url: DOCKER_HUB)
+    end
+
+    private
+
+    def credentials
+      { user: user, password: password }
+    end
+
+    def filter_tags(result, query)
+      result.select do |tag_name|
+        tag_name['name'] =~ /^#{query}/
+      end
     end
   end
 end
