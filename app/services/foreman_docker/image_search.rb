acbe85f3e715e1de51a830a121cf4432a41a2147
Sebastian Gräßl <mail@bastilian.me>
2017-02-24 12:45:51 +0100

    WIP #18518 - Introduce a ImageSearch service
    The ImageSearch service allows querying a computer resource and/or
    a registry for images and tags and returns a compiled list.

---
+++ app/services/foreman_docker/image_search.rb
@@ -0,0 +1,63 @@
+module ForemanDocker
+  class ImageSearch
+    attr_accessor :compute_resource, :registry
+
+    def initialize(compute_resource, registry)
+      @compute_resource = compute_resource
+      @registry = registry ? registry.api : Service::RegistryApi.docker_hub
+    end
+
+    def search(query)
+      return [] if query[:term].blank? || query[:term] == ':'
+
+      unless query[:tags] == 'true'
+        images(query[:term])
+      else
+        tags(query[:term])
+      end
+    end
+
+    def images(query)
+      result = []
+      result += search_compute_resource(query) if compute_resource
+      result += search_registry(query) if registry
+    end
+
+    def tags(query)
+      result = []
+      image_name, tag = query.split(':')
+      result += compute_resource_tags(image_name, tag) if compute_resource
+      result += registry_tags(image_name, tag) if registry
+      result.map { |tag_name| { 'name' => tag_name } }
+    end
+
+    def available?(query)
+      tags(query).present?
+    end
+
+    private
+
+    def search_registry(term)
+      registry.search(term)['results']
+    end
+
+    def search_compute_resource(query)
+      images = compute_resource.local_images(query)
+      images.flat_map do |image|
+        image.info['RepoTags'].map do |tag|
+          { 'name' => tag.split(':').first }
+        end
+      end.uniq
+    end
+
+    def compute_resource_tags(image_name, tag)
+      image = compute_resource.image(image_name) rescue nil
+      image ? compute_resource.tags_for_local_image(image, tag) : []
+    end
+
+    def registry_tags(image_name, tag)
+      registry.tags(image_name, tag).map { |t| t['name'] }
+    end
+  end
+end
+
---
+++ test/units/image_search_service_test.rb
@@ -0,0 +1,141 @@
+require 'test_plugin_helper'
+
+class ImageSearchServiceTest < ActiveSupport::TestCase
+  let(:compute_resource) { FactoryGirl.create(:docker_cr) }
+  let(:registry) { FactoryGirl.create(:docker_registry) }
+  let(:term) { 'centos' }
+  let(:query) { { term: term, tags: 'false' } }
+
+  subject { ForemanDocker::ImageSearch.new(compute_resource, registry) }
+
+  setup do
+    stub_registry_api
+  end
+
+  describe '#search' do
+    test 'returns {"name" => value } pairs' do
+      return_result = Hash.new
+      return_result.stubs(:info).returns({ 'RepoTags' => ["#{term}:latest"]})
+      subject.compute_resource.stubs(:local_images).with(term)
+        .returns([return_result])
+      result = subject.search(query)
+      assert_equal({"name" => term}, result.first)
+    end
+
+    context 'tags is false' do
+      test 'calls #images with term as query' do
+        subject.expects(:images).with(term).once
+          .returns([])
+        subject.search(query)
+      end
+    end
+
+    context 'tags is "true"' do
+      setup do
+        query[:tags] = 'true'
+      end
+
+      test 'calls #tags with term as query' do
+        subject.expects(:tags).with(term).once
+          .returns([])
+        subject.search(query)
+      end
+    end
+  end
+
+  describe '#images' do
+    context 'a compute_resource set' do
+      test 'calls #search_compute_resource with term as query' do
+        subject.expects(:search_compute_resource).with(term).once
+          .returns([])
+        subject.images(term)
+      end
+    end
+
+    context 'no compute_resource is set' do
+      setup { subject.compute_resource = nil }
+
+      test 'does not call #search_compute_resource' do
+        subject.expects(:search_compute_resource).with(term).never
+        subject.images(term)
+      end
+    end
+
+    context 'a registry is set' do
+      test 'calls #search_registry' do
+        subject.expects(:search_registry).with(term).once
+          .returns([])
+        subject.images(term)
+      end
+    end
+
+    context 'no registry is set' do
+      setup { subject.registry = nil }
+
+      test 'does not call #search_registry' do
+        subject.expects(:search_registry).with(term).never
+        subject.images(term)
+      end
+    end
+  end
+
+  describe '#tags' do
+    let(:tag) { 'latest' }
+    let(:query) { "#{term}:#{tag}" }
+
+    context 'a compute_resource set' do
+      test 'calls #compute_resource with image name and tag' do
+        subject.expects(:compute_resource_tags).with(term, tag).once
+          .returns([])
+        subject.tags(query)
+      end
+    end
+
+    context 'no compute_resource is set' do
+      setup { subject.compute_resource = nil }
+
+      test 'does not call #search_compute_resource' do
+        subject.expects(:compute_resource_tags).with(term, tag).never
+        subject.tags(query)
+      end
+    end
+
+    context 'a registry is set' do
+      setup { subject.compute_resource = nil }
+
+      test 'calls #registry_tags with image name and tag' do
+        subject.expects(:registry_tags).with(term, tag).once
+          .returns([])
+        subject.tags(query)
+      end
+    end
+
+    context 'no registry is set' do
+      setup { subject.registry = nil }
+      test 'does not call #registry_tags' do
+        subject.expects(:search_registry).with(term, tag).never
+        subject.images(query)
+      end
+    end
+  end
+
+  describe '#available?' do
+    test 'calls #tags with query' do
+      subject.expects(:tags).with(query).once
+        .returns([])
+      subject.available?(query)
+    end
+
+    test 'returns true if any matching image and tag is found' do
+      subject.stubs(:tags).with(query)
+        .returns([{ 'name' => "#{term}:latest" }])
+      subject.available?(query)
+    end
+
+    test 'returns false if none are found' do
+      subject.stubs(:tags).with(query)
+        .returns([])
+      subject.available?(query)
+    end
+  end
+end
