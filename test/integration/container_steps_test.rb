5cb2d52d436dff5eea3e68e9bb3e7e87f0c87135
Sebastian Gräßl <mail@bastilian.me>
2017-03-27 20:39:06 +0200

    WIP #18902 - Test registry with same tests as for hub tab

--- test/integration/container_steps_test.rb
+++ test/integration/container_steps_test.rb
@@ -3,10 +3,12 @@ require 'integration_test_helper'
 class ContainerStepsTest < IntegrationTestWithJavascript
   let(:wizard_state) { DockerContainerWizardState.create! }
   let(:compute_resource) { FactoryGirl.create(:docker_cr) }
+  let(:registry) { FactoryGirl.create(:docker_registry) }
   let(:image_search_service) { ForemanDocker::ImageSearch.new }
   let(:image_search_results) { [{'name' => 'my_fake_repository_result',
                                 'star_count' => 300,
                                 'description' => 'fake repository' }] }
+
   let(:preliminary) do
     DockerContainerWizardStates::Preliminary.create!({
       :wizard_state => wizard_state,
@@ -47,60 +49,73 @@ class ContainerStepsTest < IntegrationTestWithJavascript
   end
 
   describe 'on image step' do
-    let(:image_field_id) { 'hub_docker_container_wizard_states_image_repository_name' }
-
-    setup do
-      wizard_state.preliminary = preliminary
-      visit wizard_state_step_path(:wizard_state_id => wizard_state, :id => :image)
-    end
-
-    test 'clicking on search loads repositories' do
-      image_search_service.expects(:search).at_least(2)
-                          .returns(image_search_results)
-      fill_in image_field_id, :with => "fake"
-      wait_for_ajax
-      find('#search_repository_button_hub').click
-      wait_for_ajax
-      assert_text 'my_fake_repository_result'
-    end
-
-    describe 'autcomplete' do
-      describe 'for image name' do
-        test 'entering text triggers autocomplete' do
-          image_search_service.expects(:search)
-                              .returns(image_search_results)
-          fill_in image_field_id, :with => 'fa'
-          wait_for_ajax
-          has_selector?('.ui-autocomplete a', :text => 'my_fake_repository_result')
-        end
-
-        context 'on External Registry tab' do
-          let(:image_field_id) { 'registry_docker_container_wizard_states_image_repository_name' }
-
-          test 'it does not trigger a autocomplete' do
-            image_search_service.expects(:search).never
-            find('#registry_tab').click
-            fill_in image_field_id, :with => 'fa'
-            has_no_selector?('.ui-autocomplete a', :text => 'my_fake_repository_result')
-          end
-        end
-      end
-
-      describe 'for tag' do
-        let(:tag_field_id) { 'hub_docker_container_wizard_states_image_tag' }
-        let(:image_name) { 'fake' }
-        let(:tag_snippet) { 'lat' }
+    ['hub', 'registry'].each do |tab|
+      describe "on #{tab} tab" do
+        let(:image_field_id) { "#{tab}_docker_container_wizard_states_image_repository_name" }
+        let(:tag_field_id) { "#{tab}_docker_container_wizard_states_image_tag" }
 
         setup do
-          fill_in image_field_id, :with => 'fake'
+          registry.save
+          wizard_state.preliminary = preliminary
+          visit wizard_state_step_path(:wizard_state_id => wizard_state, :id => :image)
+          find("##{tab}_tab").click
+
+          if tab == 'registry'
+            find("#docker_container_wizard_states_image_registry_id option:last-child", :visible => false)
+              .select_option
+          end
         end
 
-        test 'it searches for tags' do
+
+        test 'clicking on search loads repositories' do
           image_search_service.expects(:search).at_least(2)
-                              .returns(['latest'])
-          fill_in tag_field_id, :with => tag_snippet
+                              .returns(image_search_results)
+          fill_in image_field_id, :with => "fake"
           wait_for_ajax
-          has_selector?('.ui-autocomplete a', :text => 'latest')
+          find("#search_repository_button_#{tab}").click
+          wait_for_ajax
+          assert_text 'my_fake_repository_result'
+        end
+
+        describe 'autcomplete' do
+          describe 'for image name' do
+            test 'entering text triggers autocomplete' do
+              image_search_service.expects(:search)
+                                  .returns(image_search_results)
+              fill_in image_field_id, :with => 'fa'
+              wait_for_ajax
+              has_selector?('.ui-autocomplete a', :text => 'my_fake_repository_result')
+            end
+
+            if tab == 'registry'
+              context 'when no registry is selected' do
+                test 'it does not trigger a autocomplete' do
+                  image_search_service.expects(:search).never
+                  find("#docker_container_wizard_states_image_registry_id option:first-child", :visible => false).select_option
+                  fill_in image_field_id, :with => 'fa'
+                  wait_for_ajax
+                  has_no_selector?('.ui-autocomplete a', :text => 'my_fake_repository_result')
+                end
+              end
+            end
+          end
+
+          describe 'for tag' do
+            let(:image_name) { 'fake' }
+            let(:tag_snippet) { 'lat' }
+
+            setup do
+              fill_in image_field_id, :with => 'fake'
+            end
+
+            test 'it searches for tags' do
+              image_search_service.expects(:search).at_least(2)
+                                  .returns(['latest'])
+              fill_in tag_field_id, :with => tag_snippet
+              wait_for_ajax
+              has_selector?('.ui-autocomplete a', :text => 'latest')
+            end
+          end
         end
       end
     end
